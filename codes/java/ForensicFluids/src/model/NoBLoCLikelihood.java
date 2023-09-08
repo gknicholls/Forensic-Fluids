dc3a19870112f3e36c77796d46195595a90abc75
jessiewu <akaruiws@gmail.com>
2023-09-08 19:27:01 +0100

    Code for implementing the NoB-LoC version of our biclustering classification.

---
+++ codes/java/ForensicFluids/src/model/NoBLoCLikelihood.java
@@ -0,0 +1,347 @@
+package model;
+
+import state.Parameter;
+import state.SubTypeList;
+import data.SingleMarkerData;
+import org.apache.commons.math3.special.Beta;
+
+public class ClusterLikelihood extends AbstractProbability {
+
+    private double logLikelihood;
+    private double storedLogLikelihood;
+    private int[][][][] eltsAllPartSetList;
+    private double[][] eltsAllPartSetPriorList;
+    private SingleMarkerData sample;
+    private Parameter alphaC;
+    private Parameter betaC;
+    private double[][] subtypeMkrLik;
+    private double[][] storedSubtypeMkrLik;
+    private SubTypeList subtypeSets;
+    private boolean updateAll;
+
+    public ClusterLikelihood(String label,
+                             int[][][][] eltsAllPartSetList,
+                             double[][] eltsAllPartSetPriorList,
+                             SingleMarkerData sample,
+                             Parameter alphaC,
+                             Parameter betaC,
+                             SubTypeList subtypeSets){
+        super(label);
+        this.eltsAllPartSetList = eltsAllPartSetList;
+        this.eltsAllPartSetPriorList = eltsAllPartSetPriorList;
+        this.sample = sample;
+        this.alphaC = alphaC;
+        this.betaC = betaC;
+        this.subtypeSets = subtypeSets;
+        subtypeMkrLik = new double[subtypeSets.getSubTypeMaxCount()][sample.getMarkerGroupCount()];
+        storedSubtypeMkrLik = new double[subtypeSets.getSubTypeMaxCount()][sample.getMarkerGroupCount()];
+        getLogLikelihood();
+        updateAll = false;
+
+    }
+
+    public ClusterLikelihood(int[][][][] eltsAllPartSetList,
+                             double[][] eltsAllPartSetPriorList,
+                             SingleMarkerData sample,
+                             Parameter alphaC,
+                             Parameter betaC,
+                             SubTypeList subtypeSets){
+        this("typeLikelihood", eltsAllPartSetList, eltsAllPartSetPriorList, sample, alphaC, betaC, subtypeSets);
+
+
+    }
+
+    public ClusterLikelihood(int[][][][] eltsAllPartSetList,
+                             double[][] eltsAllPartSetPriorList,
+                             SingleMarkerData sample,
+                             double[] alphaCValues,
+                             double[] betaCValues,
+                             SubTypeList subtypeSets){
+        this("typeLikelihood", eltsAllPartSetList, eltsAllPartSetPriorList, sample,
+                new Parameter("shape.a", alphaCValues, 0),
+                new Parameter("shape.b", betaCValues, 0),
+                subtypeSets);
+
+    }
+
+
+
+
+
+
+
+
+    public static double[] CalcIntAllPartsMkrGrpLik(int[][][] eltsAllPartSet,
+                                                    SingleMarkerData sample,
+                                                    int mkrGrpIndex,
+                                                    double alphaC,
+                                                    double betaC,
+                                                    SubTypeList subtypeSets,
+                                                    int subtypeIndex){
+
+        int partCount = eltsAllPartSet.length;
+        double priorBetaNC = Beta.logBeta(alphaC, betaC);
+
+        int s, c;
+        double logMarkerLik = 0.0;
+
+        int[] nonZeros = new int[2];
+        double[] partLikVec = new double[eltsAllPartSet.length];
+        for(int partIndex = 0; partIndex < partCount; partIndex++){
+
+            logMarkerLik = 0.0;
+
+            for(int setIndex = 0; setIndex < eltsAllPartSet[partIndex].length; setIndex++){
+                //System.out.println(partIndex + " "+ setIndex+" "+eltsAllPartSet[partIndex][setIndex]);
+
+
+                //s = CalculateAmplifiedCount(sample, mkrGrpIndex, eltsAllPartSet[partIndex][setIndex], subtypeSets, subtypeIndex);
+                nonZeros = CalculateAmplifiedCount(sample, mkrGrpIndex, eltsAllPartSet[partIndex][setIndex], subtypeSets, subtypeIndex);
+                //c = subtypeSets.getSubTypeSetSize(subtypeIndex) * eltsAllPartSet[partIndex][setIndex].length;
+                //logMarkerLik += Beta.logBeta(alphaC + s, betaC + c - s);
+                logMarkerLik += Beta.logBeta(alphaC + nonZeros[1], betaC + nonZeros[0]);
+                //System.out.println(alphaC + " "+ s +" "+betaC+" "+c);
+                //System.out.println(Beta.logBeta(alphaC + s, betaC + c - s));
+
+
+            }
+            logMarkerLik -= eltsAllPartSet[partIndex].length*priorBetaNC;
+            /*if(nonZeros[0] == 0 && nonZeros[1] ==0){
+                System.out.println("missing:"+logMarkerLik);
+            }else{
+                System.out.println(logMarkerLik);
+            }*/
+
+
+            partLikVec[partIndex] = Math.exp(logMarkerLik);
+
+        }
+
+        return partLikVec;
+
+
+    }
+
+    private static int[] CalculateAmplifiedCount(SingleMarkerData sample,
+                                                 int mkrGrpIndex,
+                                                 int[] set,
+                                                 SubTypeList subtypeSets,
+                                                 int subtypeIndex){
+        int amplified = 0;
+        int col;
+        int mkrStatus;
+        int missingCount = 0;
+        int zeroCount = 0;
+        int oneCount = 0;
+
+        for(int colIndex = 0; colIndex < set.length; colIndex++){
+
+            col = set[colIndex];
+
+
+            for(int rowIndex = 0; rowIndex < subtypeSets.getSubTypeSetSize(subtypeIndex); rowIndex++){
+
+                //System.out.println(rowIndex+" "+col+" "+sample.length+" "+subtypeIndexes.get(rowIndex));
+
+                //amplified += sample[subtypeSets.getObs(subtypeIndex, rowIndex)][col];
+                //System.out.println(subtypeIndex+" "+rowIndex);
+                mkrStatus = sample.getData(mkrGrpIndex, subtypeSets.getObs(subtypeIndex, rowIndex), col);
+                /*if(mkrStatus == -1){
+                    missingCount++;
+                }else{
+                    amplified += mkrStatus;
+                }*/
+                if(mkrStatus == 0){
+                    zeroCount++;
+                }else if(mkrStatus == 1){
+                    oneCount++;
+                }
+
+                //amplified += sample.getData(mkrGrpIndex, subtypeSets.getObs(subtypeIndex, rowIndex), col);
+            }
+
+        }
+        //System.out.println(amplified+" "+set.length+" "+subtypeIndexes.size());
+        //System.out.println(amplified+ " " + missingCount);
+
+        return new int[]{zeroCount, oneCount};
+
+    }
+
+
+    /*private static int CalculateAmplifiedCount(SingleMarkerData sample,
+                                               int mkrGrpIndex,
+                                               int[] set,
+                                               SubTypeList subtypeSets,
+                                               int subtypeIndex){
+        int amplified = 0;
+        int col;
+
+        for(int colIndex = 0; colIndex < set.length; colIndex++){
+
+            col = set[colIndex];
+
+            for(int rowIndex = 0; rowIndex < subtypeSets.getSubTypeSetSize(subtypeIndex); rowIndex++){
+
+                //System.out.println(rowIndex+" "+col+" "+sample.length+" "+subtypeIndexes.get(rowIndex));
+
+                //amplified += sample[subtypeSets.getObs(subtypeIndex, rowIndex)][col];
+                //System.out.println(subtypeIndex+" "+rowIndex);
+
+                amplified += sample.getData(mkrGrpIndex, subtypeSets.getObs(subtypeIndex, rowIndex), col);
+            }
+
+        }
+        //System.out.println(amplified+" "+set.length+" "+subtypeIndexes.size());
+
+        return amplified;
+
+    }*/
+
+
+
+
+    public double calcLogSubtypeLikelihood(int subtypeIndex){
+        double logSubtypeLikelihood = 0.0;
+        //System.out.println(subtypeIndex+" "+subtypeSets.isUpdated(subtypeIndex));
+
+        double[] colPartLik;
+        for(int mkrGrpIndex = 0; mkrGrpIndex < sample.getMarkerGroupCount(); mkrGrpIndex++){
+            if(updateAll ||
+                    alphaC.isUpdated(mkrGrpIndex) ||
+                    betaC.isUpdated(mkrGrpIndex) ||
+                    subtypeSets.isUpdated(subtypeIndex)){
+                colPartLik =
+                        CalcIntAllPartsMkrGrpLik(
+                                eltsAllPartSetList[mkrGrpIndex],
+                                sample,
+                                mkrGrpIndex,
+                                alphaC.getValue(mkrGrpIndex),
+                                betaC.getValue(mkrGrpIndex),
+                                subtypeSets,
+                                subtypeIndex);
+                subtypeMkrLik[subtypeIndex][mkrGrpIndex] = 0.0;
+                for(int partIndex = 0; partIndex < eltsAllPartSetList[mkrGrpIndex].length; partIndex++){
+
+                    subtypeMkrLik[subtypeIndex][mkrGrpIndex] += eltsAllPartSetPriorList[mkrGrpIndex][partIndex]*colPartLik[partIndex];
+
+                }
+
+            }
+
+            logSubtypeLikelihood += Math.log(subtypeMkrLik[subtypeIndex][mkrGrpIndex]);
+            //System.out.println(Math.log(subtypeMkrLik[subtypeIndex][mkrGrpIndex]));
+
+        }
+
+        return logSubtypeLikelihood;
+
+
+    }
+
+
+    public static double CalcLogTypeLikelihood(int[][][][] eltsAllPartSetList,
+                                               double[][] eltsAllPartSetPriorList,
+                                               SingleMarkerData sample,
+                                               double[] alphaC,
+                                               double[] betaC,
+                                               SubTypeList subtypeSets){
+        ClusterLikelihood likelihood = new ClusterLikelihood(eltsAllPartSetList,
+                eltsAllPartSetPriorList, sample, alphaC, betaC, subtypeSets);
+
+        return(likelihood.getLogLikelihood());
+    }
+
+    public double getLogSubtypeLikelihoods(int setIndex){
+        double temp = 0;
+        for(int mkrIndex = 0; mkrIndex < subtypeMkrLik[setIndex].length; mkrIndex++){
+            if(subtypeMkrLik[setIndex][mkrIndex] > 0){
+                temp += Math.log(subtypeMkrLik[setIndex][mkrIndex]);
+
+            }
+
+        }
+        return temp;
+    }
+
+    public void getLogSubtypeLikelihoods(double[] subtypeLikelihoods){
+
+        for(int setIndex = 0; setIndex < subtypeLikelihoods.length; setIndex++){
+
+            subtypeLikelihoods[setIndex] = 0;
+            if(subtypeSets.getSubTypeSetSize(setIndex) > 0 ){
+                for(int mkrIndex = 0; mkrIndex < subtypeMkrLik[setIndex].length; mkrIndex++){
+                    if(subtypeMkrLik[setIndex][mkrIndex] > 0){
+                        subtypeLikelihoods[setIndex] += Math.log(subtypeMkrLik[setIndex][mkrIndex]);
+
+                    }
+                }
+            }
+        }
+
+    }
+
+
+
+    public double getLogLikelihood(){
+        logLikelihood = 0.0;
+
+        for(int subtypeIndex = 0; subtypeIndex < subtypeSets.getSubTypeMaxCount(); subtypeIndex++){
+            //System.out.println(subtypeIndex);
+            //if(subtypeSets[subtypeIndex].size() > 0){
+            //System.out.print(subtypeSets.getSubTypeSetSize(subtypeIndex)+" ");
+            if(subtypeSets.getSubTypeSetSize(subtypeIndex) > 0){
+
+                //logLikelihood += calcLogSubtypeLikelihood(eltsAllPartSetList,
+                //        eltsAllPartSetPriorList, sample, alphaC, betaC, subtypeSets, subtypeIndex);
+
+                logLikelihood += calcLogSubtypeLikelihood(subtypeIndex);
+            }
+
+        }
+
+        return(logLikelihood);
+    }
+
+    public boolean isUpdated(){
+        //System.out.println("alpha: "+alphaC.isUpdated());
+        //System.out.println("beta: "+betaC.isUpdated());
+        //System.out.println("subtype: "+subtypeSets.isUpdated());
+        return (alphaC.isUpdated() || betaC.isUpdated() || subtypeSets.isUpdated());
+    }
+
+    public boolean isAllUpdated(){
+        return updateAll;
+    }
+
+    public void setUpdateAll(boolean update){
+        updateAll = update;
+    }
+
+    public void store(){
+        storedLogLikelihood = logLikelihood;
+        for(int typeIndex = 0; typeIndex < storedSubtypeMkrLik.length; typeIndex++){
+            System.arraycopy(subtypeMkrLik[typeIndex], 0,
+                    storedSubtypeMkrLik[typeIndex], 0,
+                    subtypeMkrLik[typeIndex].length);
+        }
+        //System.out.println("stored-lik"+storedLogLikelihood);
+        updateAll = false;
+
+    }
+
+    public void restore(){
+        logLikelihood = storedLogLikelihood;
+        double[][] tmp = subtypeMkrLik;
+        subtypeMkrLik = storedSubtypeMkrLik;
+        storedSubtypeMkrLik = tmp;
+    }
+
+    public String log(){
+        return ""+ logLikelihood;
+    }
+
+    public String logStored(){
+        return ""+ storedLogLikelihood;
+    }
+}
---
+++ codes/java/ForensicFluids/src/test/TestNoBLoCLikelihood.java
@@ -0,0 +1,410 @@
+package test;
+
+import state.Parameter;
+import state.SubTypeList;
+import data.SingleMarkerData;
+import junit.framework.TestCase;
+import model.ClusterLikelihood;
+//import model.OldClusterLikelihood;
+import utils.MathUtils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TestClusterLikelihood extends TestCase {
+
+    public interface Instance {
+
+        int[][][] getPartitions();
+
+        int[][] getData();
+
+        double[] getExpectedResult();
+
+        double getAlpha();
+
+        double getBeta();
+
+        double[] getColPrior();
+
+        ArrayList<Integer> getSubtypeIndexes();
+    }
+
+    protected Instance test0 = new Instance() {
+
+        @Override
+        public int[][][] getPartitions() {
+
+            int[][][] partition = {
+                    {{0, 1, 2}},
+                    {{0, 1}, {2}},
+                    {{0, 2}, {1}},
+                    {{0}, {1, 2}},
+                    {{0}, {1}, {2}}
+
+            };
+
+            return partition;
+        }
+
+        @Override
+        public int[][] getData(){
+
+            int[][] slvEx1Mat = {
+                    {0, 1, 1},
+                    {0, 1, 1},
+                    {1, 0, 0},
+                    {0, 0, 0},
+                    {0, 0, 1},
+                    {0, 0, 1},
+                    {0, 0, 1},
+                    {0, 0, 0},
+                    {0, 0, 0},
+                    {0, 0, 0}
+            };
+
+            return slvEx1Mat;
+        }
+
+
+
+        @Override
+        public double[] getExpectedResult() {
+            return new double[]{
+                    9.15135378264343e-09,
+                    2.82044324957933e-08,
+                    6.11241571116264e-09,
+                    1.12060954704648e-08,
+                    1.64505988352471e-08
+            };
+        }
+
+        @Override
+        public double[] getColPrior() {
+            return new double[]{
+                    0.1648351648351658,
+                    0.2197802197802201,
+                    0.2197802197802201,
+                    0.2197802197802201,
+                    0.1758241758241763
+            };
+        }
+
+        @Override
+        public double getAlpha(){
+            return 2;
+        }
+
+        @Override
+        public double getBeta(){
+            return 3;
+        }
+
+        @Override
+        public ArrayList<Integer> getSubtypeIndexes(){
+            ArrayList<Integer> subtypeIndexes = new ArrayList<Integer>(
+                    Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+            );
+            return subtypeIndexes;
+        }
+    };
+
+    protected Instance test1 = new Instance() {
+
+        @Override
+        public int[][][] getPartitions() {
+
+            int[][][] partition = {
+                    {{0, 1, 2, 3}},
+                    {{0, 1, 2}, {3}},
+                    {{0, 1, 3}, {2}},
+                    {{0, 1}, {2, 3}},
+                    {{0, 1}, {2}, {3}},
+                    {{0, 2, 3}, {1}},
+                    {{0, 2}, {1, 3}},
+                    {{0, 2}, {1}, {3}},
+                    {{0, 3}, {1, 2}},
+                    {{0}, {1, 2, 3}},
+                    {{0}, {1, 2}, {3}},
+                    {{0, 3}, {1}, {2}},
+                    {{0}, {1, 3}, {2}},
+                    {{0}, {1}, {2, 3}},
+                    {{0}, {1}, {2}, {3}}
+            };
+
+            return partition;
+        }
+
+        @Override
+        public int[][] getData(){
+
+            int[][] slvEx2Mat = {
+                    {1, 1, 1, 1},
+                    {1, 1, 1, 1},
+                    {1, 1, 0, 1},
+                    {1, 1, 0, 1},
+                    {1, 1, 0, 1},
+                    {1, 1, 0, 1},
+                    {1, 1, 0, 1},
+                    {0, 1, 0, 1},
+                    {0, 1, 0, 0},
+                    {0, 1, 0, 1}
+            };
+
+            return slvEx2Mat;
+        }
+
+
+
+        @Override
+        public double[] getExpectedResult() {
+            return new double[]{
+                    12.0*6.213567834736545e-13,
+                    144.0*7.590906346753908e-14,
+                    144.0*1.238007816916047e-11,
+                    144.0*1.713712134340804e-13,
+                    1728.0*4.304705814376286e-13,
+                    144.0*2.819479500222888e-13,
+                    144.0*5.075224397855477e-12,
+                    1728.0*1.550575742854044e-13,
+                    144.0*6.31367628441347e-14,
+                    144.0*3.17437901773346e-14,
+                    1728.0*4.664541904122907e-15,
+                    1728.0*6.796903917436222e-13,
+                    1728.0*1.643614947307302e-12,
+                    1728.0*1.999089387481247e-14,
+                    20736.0*5.021550316009523e-14
+            };
+        }
+
+        @Override
+        public double[] getColPrior() {
+            return new double[]{
+                    35.0/(3.0 * 17.0 * 19.0),
+                    20.0/(17.0 * 19.0),
+                    20.0/(17.0 * 19.0),
+                    50.0/(3.0 * 17.0 * 19.0),
+                    80.0/(3.0 * 17.0 * 19.0),
+                    20.0/(17.0 * 19.0),
+                    50.0/(3.0 * 17.0 * 19.0),
+                    80.0/(3.0 * 17.0 * 19.0),
+                    50.0/(3.0 * 17.0 * 19.0),
+                    20.0/(17.0 * 19.0),
+                    80.0/(3.0 * 17.0 * 19.0),
+                    80.0/(3.0 * 17.0 * 19.0),
+                    80.0/(3.0 * 17.0 * 19.0),
+                    80.0/(3.0 * 17.0 * 19.0),
+                    64.0/(3.0 * 17.0 * 19.0)
+
+            };
+        }
+
+        @Override
+        public double getAlpha(){
+            return 3;
+        }
+
+        @Override
+        public double getBeta(){
+            return 2;
+        }
+
+        @Override
+        public ArrayList<Integer> getSubtypeIndexes(){
+            ArrayList<Integer> subtypeIndexes = new ArrayList<Integer>(
+                    Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+            );
+            return subtypeIndexes;
+        }
+    };
+
+    public void testCalcIntAllPartsMkrGrpLik (){
+        Instance[] tests = new Instance[]{test0, test1};
+        for(Instance test: tests) {
+
+            int[][][] partitions = test.getPartitions();
+            //int[][] data = test.getData();
+            double alpha = test.getAlpha();
+            double beta = test.getBeta();
+            double[] mkrGrpLik;
+            double[] expectedResult = test.getExpectedResult();
+
+            ArrayList<Integer>[] subtypeList = (ArrayList<Integer>[]) new ArrayList[]{test.getSubtypeIndexes()};
+            SingleMarkerData data = new SingleMarkerData(new int[][][]{test.getData()});
+            SubTypeList subTypeList = new SubTypeList(subtypeList );
+
+
+            mkrGrpLik = ClusterLikelihood.CalcIntAllPartsMkrGrpLik(
+                    partitions, data, 0, alpha, beta, subTypeList,0);
+
+            for (int partIndex = 0; partIndex < partitions.length; partIndex++) {
+
+                assertEquals(mkrGrpLik[partIndex], expectedResult[partIndex], 1e-10);
+                assertEquals(Math.log(mkrGrpLik[partIndex]), Math.log(expectedResult[partIndex]), 1e-10);
+            }
+        }
+    }
+
+    public void testCalcLogSubtypeLikelihood(){
+        int[][][][] mkrGrpPartitions = new int[2][][][];
+        mkrGrpPartitions[0] = test0.getPartitions();
+        mkrGrpPartitions[1] = test1.getPartitions();
+
+        int[][][] dataMat = new int[2][][];
+        dataMat[0] = test0.getData();
+        dataMat[1] = test1.getData();
+        SingleMarkerData data = new SingleMarkerData(dataMat);
+
+        double[][] colPriors = new double[2][];
+        colPriors[0] = test0.getColPrior();
+        colPriors[1] = test1.getColPrior();
+        double[] alphaCValues = new double[]{test0.getAlpha(), test1.getAlpha()};
+        double[] betaCValues = new double[]{test0.getBeta(), test1.getBeta()};
+
+        ArrayList<Integer>[] subtypeSets = (ArrayList<Integer>[]) new ArrayList[]{test1.getSubtypeIndexes()};
+
+        SubTypeList subTypeList = new SubTypeList(subtypeSets);
+
+        Parameter alphaC = new Parameter("shape.a", alphaCValues, 0);
+        Parameter betaC = new Parameter("shape.b", betaCValues, 0);
+        ClusterLikelihood likelihood = new ClusterLikelihood(mkrGrpPartitions,
+                colPriors, data, alphaC, betaC, subTypeList);
+
+        //double logSubLik = likelihood.calcLogSubtypeLikelihood(0);
+
+        //assertEquals(logSubLik, -49.6924097777349, 1e-10);
+
+    }
+
+    public void testCalcLogTypeLikelihood(){
+        String filepath = "/Users/chwu/Documents/research/bfc/github/Forensic-Fluids/output/ex.type.log.res.csv";
+        double[][] logTypeLik = getTable(filepath, 115975);
+
+        int[][][][] mkrGrpPartitions = new int[2][][][];
+        mkrGrpPartitions[0] = test0.getPartitions();
+        mkrGrpPartitions[1] = test1.getPartitions();
+
+        int[][][] dataMat = new int[2][][];
+        dataMat[0] = test0.getData();
+        dataMat[1] = test1.getData();
+        SingleMarkerData data = new SingleMarkerData(dataMat);
+
+        double[][] colPriors = new double[2][];
+        colPriors[0] = test0.getColPrior();
+        colPriors[1] = test1.getColPrior();
+        double[] alphaCValues = new double[]{test0.getAlpha(), test1.getAlpha()};
+        double[] betaCValues = new double[]{test0.getBeta(), test1.getBeta()};
+
+
+        ArrayList<Integer>[] subtypeParts;
+        double logSubLik;
+
+        String clustFile = "/Users/chwu/Documents/research/bfc/github/Forensic-Fluids/output/allPartitionSets10.txt";
+
+        int setMaxCount = 10;
+        ArrayList<Integer>[][] allParts10 = getCluster(clustFile, 115975);
+
+        for(int partIndex = 0; partIndex < allParts10.length; partIndex++){
+            //System.out.println("partIndex: "+ partIndex);
+            subtypeParts = (ArrayList<Integer>[]) new ArrayList[setMaxCount];
+            for(int setIndex = 0; setIndex < subtypeParts.length; setIndex++){
+                subtypeParts[setIndex] = new ArrayList<Integer>();
+            }
+            int[] samples = MathUtils.sample(allParts10[partIndex].length, 0,setMaxCount -1);
+            for(int setIndex = 0; setIndex < allParts10[partIndex].length; setIndex++){
+                //System.out.println(samples[setIndex]);
+                subtypeParts[samples[setIndex]] = allParts10[partIndex][setIndex];
+            }
+
+            SubTypeList subTypeList = new SubTypeList(subtypeParts);
+
+            Parameter alphaC = new Parameter("shape.a", alphaCValues, 0);
+            Parameter betaC = new Parameter("shape.b", betaCValues, 0);
+            ClusterLikelihood likelihood = new ClusterLikelihood(mkrGrpPartitions,
+                    colPriors, data, alphaC, betaC, subTypeList);
+
+            logSubLik = likelihood.getLogLikelihood();
+
+            assertEquals(logSubLik, logTypeLik[partIndex][0], 1e-10);
+        }
+
+
+    }
+
+    private static double[][] getTable(String file, int lineCount){
+        try{
+
+            BufferedReader tableReader = new BufferedReader(new FileReader(file));
+            String line = tableReader.readLine();
+            String[] elts = line.split(",");
+            double[][] table = new double[lineCount][elts.length];
+
+            for(int lineIndex = 0; lineIndex < lineCount; lineIndex++){
+                elts = tableReader.readLine().split(",");
+                for(int eltsIndex = 0; eltsIndex < elts.length; eltsIndex++){
+                    table[lineIndex][eltsIndex] = Double.parseDouble(elts[eltsIndex]);
+                }
+            }
+
+            tableReader.close();
+
+            return table;
+
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static ArrayList<Integer>[][] getCluster(String file, int lineCount){
+        try{
+
+            BufferedReader clustReader = new BufferedReader(new FileReader(file));
+            String line = "";
+            String[] clustStr;
+            String[] obsInClust;
+            ArrayList<Integer>[][] clusts = (ArrayList<Integer>[][]) new ArrayList[lineCount][];
+
+            for(int lineIndex = 0; lineIndex < lineCount; lineIndex++){
+                line = clustReader.readLine().trim();
+                line = line.substring(1, line.length() - 1);
+
+                if(line.contains("], [")){
+                    clustStr = line.split("\\], \\[");
+                }else{
+                    clustStr = new String[]{line};
+                }
+
+
+                clusts[lineIndex] = (ArrayList<Integer>[]) new ArrayList[clustStr.length];
+                for(int clustIndex = 0; clustIndex < clustStr.length; clustIndex++){
+                    obsInClust = clustStr[clustIndex].replaceAll("\\[|\\]", "").split(", ");
+
+                    clusts[lineIndex][clustIndex] = new ArrayList<Integer>();
+                    for(int obsIndex = 0; obsIndex < obsInClust.length; obsIndex++){
+                        clusts[lineIndex][clustIndex].add(Integer.parseInt(obsInClust[obsIndex]));
+
+                    }
+
+
+
+                }
+            }
+
+            clustReader.close();
+
+            return clusts;
+
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+
+
+
+}
